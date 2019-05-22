d3157da7732b445ecd75764e3f33f3214a7805da
To-D <1244467220@qq.com>
2019-05-22 11:03:48 +0800

    lab8

--- lab8/lab8.php
+++ lab8/lab8.php
@@ -38,13 +38,14 @@ $grandTotal = 0;
 <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer
             mdl-layout--fixed-header">
 
+    <?php include 'left.inc.php';?>
+    <?php include 'header.inc.php'; ?>
   <main class="mdl-layout__content mdl-color--grey-50">
     <header class="mdl-color--blue-grey-200">
       <h4>Order Summaries</h4>
       <p>Examine your customer orders</p>
     </header>   
     <section class="page-content">
-    
         <div class="mdl-grid">
 
           <!-- mdl-cell + mdl-card -->
@@ -68,7 +69,6 @@ $grandTotal = 0;
 
 
 
-
           <!-- mdl-cell + mdl-card -->
           <div class="mdl-cell mdl-cell--9-col card-lesson mdl-card  mdl-shadow--2dp">
             <div class="mdl-card__title mdl-color--orange">
@@ -89,15 +89,26 @@ $grandTotal = 0;
                   <tfoot>
                       <tr class="totals">
                           <td colspan="4">Subtotal</td>
-                          <td>$<?php echo number_format($subtotal,2); ?></td>
+                          <td>$<?php
+                              $subtotal=$quantity1*$price1+$quantity2*$price2+$quantity3*$price3+$quantity4*$price4;
+                              echo  number_format($subtotal,2);
+                              ?></td>
                       </tr>
                       <tr class="totals">
                           <td colspan="4">Shipping</td>
-                          <td>$<?php echo number_format($shipping,2); ?></td>
+                          <td>$<?php
+                              $shipping=200;
+                              if($subtotal>$shippingThreshold)
+                                  $shipping=100;
+                              echo number_format($shipping,2);
+                              ?></td>
                       </tr> 
                       <tr class="grandtotals">
                           <td colspan="4">Grand Total</td>
-                          <td>$<?php echo number_format($grandTotal,2); ?></td>
+                          <td>$<?php
+                              $grandTotal=$shipping+$subtotal;
+                              echo number_format($grandTotal,2);
+                              ?></td>
                       </tr>                            
                   </tfoot>          
                   <tbody>
